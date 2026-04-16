ea3cb35059b8744b689a533ac0c7147d557075b5
Milad <miladposhtkohi@gmail.com>
2026-04-16 10:08:51 +0200

    update authMiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,16 +1,19 @@
 import jwt from "jsonwebtoken";
 
-
 export default function authMiddleware(req, res, next) {
-    const token = req.header("Authorization").replace("Bearer ", "");
-    if (!token) {
-        return res.status(401).json({ message: "No token, authorization denied" });
-    }
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch (err) {
-        res.status(400).json({ message: "Token is not valid" });
-    }
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // <-- här skapas req.user
+    next();
+  } catch (err) {
+    res.status(400).json({ message: "Token is not valid" });
+  }
 }
